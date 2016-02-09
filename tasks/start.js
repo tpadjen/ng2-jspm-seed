c35859ef41f783ec4e36fd13a41e3ac6e223adf3
Tim Padjen <tpadjen@gmail.com>
2016-02-09 00:21:55 -0700

    Remove start task

--- tasks/start.js
+++
@@ -1,7 +0,0 @@
-var hs = require("http-server");
-var open = require('open');
-var chokidar = require('chokidar-socket-emitter');
-var server = hs.createServer({"root": process.cwd()});
-chokidar({app: server.server, dir: process.cwd(), path: process.cwd()});
-server.listen(8080);
-open("http://127.0.0.1:8080");
