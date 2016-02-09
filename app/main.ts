f2eafbbe3c394e295369f19af92d54906cd88b11
Tim Padjen <tpadjen@gmail.com>
2016-02-09 02:05:08 -0700

    Remove unnecessary shims

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
-import "../shims";
+import 'reflect-metadata';
+
 import {bootstrap} from 'angular2/bootstrap';
 import {AppComponent} from './app.component';
 import "./index.css";
--- shims.js
+++
@@ -1,5 +0,0 @@
-import 'reflect-metadata';
-import Zone from 'zone.js';
-
-// workaround for https://github.com/angular/angular/issues/6007
-window.Zone = Zone;
